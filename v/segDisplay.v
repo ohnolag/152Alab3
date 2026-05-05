397c2fb2fe24cfcd794b97bd61ba2586d3d46d33
Owen Johnson <orjfalcon@gmail.com>
2026-05-05 12:53:05 -0700

    added 7-seg display interface following basys spec

---
+++ v/segDisplay.v
@@ -0,0 +1,15 @@
+module segDisplay (
+    output AN0,
+    output AN1,
+    output AN2,
+    output AN3,
+    output CA,
+    output CB,
+    output CC,
+    output CD,
+    output CE,
+    output CF,
+    output CG
+);
+    
+endmodule
